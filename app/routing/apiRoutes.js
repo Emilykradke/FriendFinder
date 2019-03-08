59f19688942ea8e4ebaa52d277eb235ae8715ab7
Emilykradke <emilykradke@gmail.com>
2019-03-07 21:42:05 -0600

    comment

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,31 @@ module.exports = function(app) {
     });
 
     app.post("/api/friendList", function(req, res) {
-        console.log(req.body)
+        var newUserScores = req.body.scores;
+        var scoresArray = [];
+        var bestMatch = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+
+            var diffInScore = 0;
+
+            for (var j = 0; j < newUserScores.length; j++) {
+                diffInScore += (Math.abs(parseInt(friends[i].scores[j] - parseInt(newUserScores[j]))));
+            }
+
+            // push results into "scoresArray var"
+            scoresArray.push(diffInScore);
+        }
+
+        for(var i = 0; i < scoresArray.length; i++){
+            if(scoresArray[i] <= scoresArray[bestMatch]){
+                bestMatch = i;
+            } 
+        }
+    
+        var newFriend = friends[bestMatch];
+        res.json(newFriend);
+        // res.end()
     });
     
 }
